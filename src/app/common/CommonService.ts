395d5a5a1a3949d454ae642b34a883f2d07b0690
GitKavindu <bakh99810@gmail.com>
2025-06-23 23:13:40 +0530

    Made the three seats display formats

--- src/app/common/CommonService.ts
+++ src/app/common/CommonService.ts
@@ -9,31 +9,15 @@ export class CommonService{
       
         let seatModel:SeatModel[]=new Array()
 
-        // for(let i=0;i<seat.length;i++){
-        //     if(seat[i].apartmentId==apartmentId){
-        //         let tempSeat:CompartmentSeatModel=new CompartmentSeatModel()
-        //         tempSeat.number=seat[i].rowNo
-        //         tempSeat.available=
-
-        //         let found:boolean=false
-
-        //         for(let j=0;j<seatModel.length;j++){
-        //             if(seatModel[j].row==seat[i].rowNo){
-        //                 found=true
-        //                 if(seat[i].isLeft==true){
-        //                     seatModel[j].left.push()
-        //                 }
-        //             }
-        //         }
-        //     }
-                   
-        // }
-
         let j:number=0
         let tempSeat:SeatModel=new SeatModel()
         tempSeat.left=[]
         tempSeat.right=[]
-        
+
+        let maxRowNo=0
+        let leftMaxNo=0
+        let rightMaxNo=0
+
         for(let i=0;i<seat.length;i++){
             if(seat[i].rowNo!=j){
                 if(j!=0){
@@ -56,10 +40,63 @@ export class CommonService{
                 tempSeat.left.push(compSeat) 
             else
                 tempSeat.right.push(compSeat) 
+
+            //find largest row no
+            if(maxRowNo<seat[i].rowNo)
+                maxRowNo=seat[i].rowNo
+
+            //find largest left seqno
+            if(seat[i].isLeft==true){
+                if(leftMaxNo<seat[i].seqNo)
+                    leftMaxNo=seat[i].seqNo
+            }
+            //find largest right seqno
+            else{
+                if(rightMaxNo<seat[i].seqNo)
+                    rightMaxNo=seat[i].seqNo
+            }
         }
          
         tempSeat.row=j
         seatModel.push(tempSeat)
+
+        //fill the gaps
+        for(let i=1;i<=maxRowNo;i++){
+
+            //Add a row if there is a gap in rows
+            if(seatModel[i-1].row!=i){
+                
+                tempSeat=new SeatModel()
+                tempSeat.left=[]
+                tempSeat.right=[]
+                seatModel.splice(i-1,0,tempSeat)
+            }
+
+            //Add a seat to left if there is a gap in left seats
+            for(let k=1;k<=leftMaxNo;k++){
+                if(seatModel[i-1].left[k-1]==undefined || seatModel[i-1].left[k-1].number!=k){
+                    let compSeat:CompartmentSeatModel=new CompartmentSeatModel()
+                    compSeat.available=false
+                    compSeat.selected=false
+                    compSeat.number=k
+                    seatModel[i-1].left.splice(k-1,0,compSeat)
+                }
+            }
+
+            //Add a seat to right if there is a gap in right seats
+            for(let k=1;k<=rightMaxNo;k++){ 
+                if(seatModel[i-1].right[k-1]==undefined || seatModel[i-1].right[k-1].number!=k){
+                    let compSeat:CompartmentSeatModel=new CompartmentSeatModel()
+                    compSeat.available=false
+                    compSeat.selected=false
+                    compSeat.number=k
+                    seatModel[i-1].right.splice(k-1,0,compSeat)
+                }
+            }
+
+        }
+
+       
         return seatModel
     }
 }
--- src/app/pages/apartment/train-compartment/train-compartment.component.ts
+++ src/app/pages/apartment/train-compartment/train-compartment.component.ts
@@ -9,6 +9,7 @@ import { SeatModel } from '../../../../Models/SeatModel';
 })
 export class TrainCompartmentComponent {
 
+  @Input() type:number=1
   @Input() seat :SeatModel[] | undefined
   seats!:SeatModel[]
 
@@ -16,136 +17,6 @@ export class TrainCompartmentComponent {
     if(this.seat!=undefined)
        this.seats=this.seat
   }
-
-//  seats = [
-//     // Upper berth - left side
-//     { number: '1A', available: true, selected: false, isLeft: true, section: 'upper' },
-//     { number: '1B', available: true, selected: false, isLeft: true, section: 'upper' },
-//     // Upper berth - right side
-//     { number: '1C', available: false, selected: false, isLeft: false, section: 'upper' },
-//     { number: '1D', available: true, selected: false, isLeft: false, section: 'upper' },
-    
-//     // Lower berth - left side
-//     { number: '2A', available: true, selected: false, isLeft: true, section: 'lower' },
-//     { number: '2B', available: true, selected: false, isLeft: true, section: 'lower' },
-//     // Lower berth - right side
-//     { number: '2C', available: true, selected: false, isLeft: false, section: 'lower' },
-//     { number: '2D', available: false, selected: false, isLeft: false, section: 'lower' },
-//     // New seat
-//     //{ number: '2F', available: false, selected: false, isLeft: false, section: 'lower' },
-//     //   { number: '2G', available: false, selected: false, isLeft: false, section: 'lower' },
-    
-//     // Side berth - left side
-//     { number: '3A', available: true, selected: false, isLeft: true, section: 'side' },
-//     { number: '3B', available: false, selected: false, isLeft: true, section: 'side' },
-//     // Side berth - right side
-//     { number: '3C', available: true, selected: false, isLeft: false, section: 'side' },
-//     { number: '3D', available: true, selected: false, isLeft: false, section: 'side' },
-//     // { number: '2E', available: false, selected: false, isLeft: false, section: 'lower' },
-//   ];
-
-  // seats :SeatModel[]= [
-  //   {
-  //     row:1,
-  //     left:[{ 
-  //             number:1,
-  //             available:true,
-  //             selected:false
-  //           },
-  //           { 
-  //             number:2,
-  //             available:true,
-  //             selected:false
-  //           }
-  //          ],
-  //     right:[{ 
-  //             number:1,
-  //             available:true,
-  //             selected:false
-  //           },
-  //           { 
-  //             number:2,
-  //             available:true,
-  //             selected:false
-  //           }
-  //          ]
-  //   },
-  //   {
-  //     row:2,
-  //     left:[{ 
-  //             number:1,
-  //             available:true,
-  //             selected:false
-  //           },
-  //           { 
-  //             number:2,
-  //             available:true,
-  //             selected:false
-  //           }
-  //          ],
-  //     right:[{ 
-  //             number:1,
-  //             available:true,
-  //             selected:false
-  //           },
-  //           { 
-  //             number:2,
-  //             available:true,
-  //             selected:false
-  //           }
-  //          ]
-  //   },
-  //   {
-  //     row:3,
-  //     left:[{ 
-  //             number:1,
-  //             available:true,
-  //             selected:false
-  //           },
-  //           { 
-  //             number:2,
-  //             available:false,
-  //             selected:false
-  //           }
-  //          ],
-  //     right:[{ 
-  //             number:1,
-  //             available:true,
-  //             selected:false
-  //           },
-  //           { 
-  //             number:2,
-  //             available:true,
-  //             selected:false
-  //           }
-  //          ]
-  //   },
-  //   {
-  //     row:4,
-  //     left:[{ 
-  //             number:1,
-  //             available:false,
-  //             selected:false
-  //           },
-  //           { 
-  //             number:2,
-  //             available:true,
-  //             selected:false
-  //           }
-  //          ],
-  //     right:[{ 
-  //             number:1,
-  //             available:true,
-  //             selected:false
-  //           },
-  //           { 
-  //             number:2,
-  //             available:true,
-  //             selected:false
-  //           }
-  //          ]
-  //   }
-  // ]
 
   selectedSeats: string[] = [];
 
@@ -163,15 +34,30 @@ export class TrainCompartmentComponent {
   }
 
   getSeatClass(seat: any): string {
-    if (!seat.available) return 'bg-secondary text-white';
-    return seat.selected ? 'bg-primary text-white' : 'bg-light';
+    if(this.type==3)
+      return 'bg-secondary text-white fake-disabled';
+    else if (!seat.available)
+      return 'bg-secondary text-white';
+
+    if(this.type==1)
+      return seat.selected ? 'bg-primary text-white' : 'bg-light';
+    else
+      return seat.selected ? 'bg-third text-white' : 'bg-light';
   }
 
-  // Helper to get seats for a specific section and side
-  // getSeats(section: string, isLeft: boolean): any[] {
-  //   return this.seats.filter(seat => 
-  //     seat.section === section && seat.isLeft === isLeft
-  //   );
-  // }
+  getLabelClass():string{
+    if(this.type==1)
+      return 'badge bg-primary mr-2 text-white';
+    else
+      return 'badge mr-2 bg-third text-white';
+  }
+
+  getRightText(){
+    if(this.type==1)
+      return 'Booked'
+    else
+      return 'Already Disabled'
+  }
+  
 } 
     
